09a1dcc9f355483abcf529cb521a842db7231ae2
raushan.kumar <raushan.kumar@diatoz.com>
2024-05-15 18:59:41 +0530

    package changes updated

---
+++ src/app/components/PrivacyPolicy/Privacy.tsx
@@ -0,0 +1,238 @@
+import React from "react";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import "./privacy.scss";
+import Link from "next/link";
+
+const Privacy = () => {
+  return (
+    <div>
+      <div id="outer-section-privacy-policy">
+        <div className="heding-section">
+          <span className="home">
+            <Link href="/" className="link-home">
+              Home
+            </Link>
+          </span>
+          <ArrowForwardIosOutlinedIcon fontSize="small" /> Privacy Policy
+        </div>
+        <div className="heding-text"> Privacy Policy</div>
+        <div className="heading-below">
+          This Privacy Policy was last updated on April 21, 2024
+        </div>
+        <div className="outer-section-privacy">
+          <div className="inner-text">
+            <div className="text">
+              Hello! ELA Learn Powered by Digitide Education, (Digitide
+              Education Private Limited or DEPL), is committed to keeping any
+              and all personal information collected of those individuals who
+              visit our website and make use of our online facilities and
+              services accurate, confidential, secure and private.
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">
+                Scope of this Privacy Policy
+              </div>
+              <ul>
+                <li>
+                  Our privacy policy has been designed and created to ensure
+                  those affiliated with our commitment and realization of our
+                  obligation not only to meet but to exceed most existing
+                  privacy standards. THEREFORE, this Privacy Policy Agreement
+                  shall apply to Digitide Education Private Limited, and thus it
+                  shall govern any and all data collection and usage thereof.{" "}
+                </li>
+                <li>
+                  Through the use of{" "}
+                  <Link href="https://elalearn.com" className="privacy-anchor">
+                    elalearn.com
+                  </Link>
+                  , you are herein consenting to the following data procedures
+                  expressed within this agreement.
+                </li>
+                <li>
+                  Through your use of the Website, Products & Services, you may
+                  provide us with certain information, By using the Website,
+                  Products & Services, you authorise TLSPL to use your
+                  information in India or any other country where we may
+                  operate.
+                </li>
+                <li>
+                  By agreeing to use our product & services you agree to share
+                  your data for our various business operations. Our customer
+                  service operators might call you to train & educate you about
+                  upcoming & existing product/service features.{" "}
+                </li>
+                <li>
+                  After completion of the registration data and creation of your
+                  account. You shall be responsible for maintaining the secrecy
+                  and confidentiality of your password and for all activities
+                  that transpire on or within your account.
+                </li>
+                <li>
+                  It shall be your responsibility to notify TLSPL immediately to
+                  report any unauthorized access or use of your account or
+                  password or any other breach of security.
+                </li>
+                <li>
+                  TLSPL shall not be held liable for any loss and/or damage
+                  arising from any failure to comply with the terms and/or
+                  conditions of the Terms of Service.
+                </li>
+              </ul>
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">
+                Information that we may collect or receive{" "}
+              </div>
+              <ul>
+                <li>
+                  When you register for an account, you may provide us with a
+                  valid email address and may provide us with additional
+                  information, such as your name or billing information.{" "}
+                </li>
+                <li>
+                  Depending on how you use our Website, Products & Services, we
+                  may also receive information from external applications you
+                  use to access our Website, Products & Services or we may
+                  receive information through various web technologies, such as
+                  cookies, log files, clear gifs, web beacons and any others
+                </li>
+              </ul>
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">
+                How we use information{" "}
+              </div>
+              <ul>
+                <li>
+                  We use the Information gathered from you for the purpose of
+                  analytics and continuous improvement of our Website, Products
+                  & Services.{" "}
+                </li>
+                <li>
+                  The information received through the usage of our Website,
+                  Products & Services, feedback, or through email communication
+                  is further used to improve your experience of all our products
+                  and services.{" "}
+                </li>
+                <li>
+                  We may also track certain passive information received to
+                  improve our marketing, and business analytics, and for this,
+                  we may need to work with third-party providers if and when we
+                  feel it is fit and necessary
+                </li>
+              </ul>
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">
+                How you can protect your information{" "}
+              </div>
+              <ul>
+                <li>
+                  If you would like to disable our access to any passive
+                  information, we receive from the use of various technologies,
+                  you may choose to disable cookies in your web browser, which
+                  may affect the complete use of our Website, Products &
+                  Service.{" "}
+                </li>
+                <li>
+                  Please be aware that TSLPL will still receive information
+                  about your usage and also about you that you have provided
+                  such as your email address.{" "}
+                </li>
+                <li>
+                  If you choose to terminate your account, TLSPL warstore
+                  information about you for a period of 180 Days, after which
+                  time the information may be deleted partially or fully as
+                  deemed fit at the given time.{" "}
+                </li>
+              </ul>
+            </div>
+
+            <div className="text">
+              <div className="privacy-text-heading">
+                Links to Other Websites
+              </div>
+              <ul>
+                <li>
+                  Our website does contain links to other websites. The Digitide
+                  Education Private Limited does not claim nor accept
+                  responsibility for any privacy policies, practices, and/or
+                  procedures of other such websites. Therefore, we encourage all
+                  users and visitors to be aware when they leave our website and
+                  to read the privacy statements of each and every website that
+                  collects personally identifiable information.{" "}
+                </li>
+                <li>
+                  The aforementioned Privacy Policy Agreement applies only and
+                  solely to the information collected by our website. The
+                  security Digitide Education Private Limited shall endeavour
+                  and shall take every precaution to maintain adequate physical,
+                  procedural and technical security with respect to our offices
+                  and information storage facilities so as to prevent any loss,
+                  misuse, unauthorized access, disclosure or modification of the
+                  user's personal information under our control.
+                </li>
+                <li>
+                  The company also uses a Secure Socket Layer (SSL) for
+                  authentication and private communications in an effort to
+                  build users' trust and confidence in the Internet and website
+                  use by providing simple and secure access and communication of
+                  credit card and personal information.
+                </li>
+              </ul>
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">
+                Changes to the Privacy Policy Agreement
+              </div>
+              The Digitide Education Private Limited reserves the right to
+              update and/or change the terms of our privacy policy, and as such
+              we will post those changes to our website homepage at{" "}
+              <Link href="https://elalearn.com" className="privacy-anchor">
+                elalearn.com
+              </Link>{" "}
+              so that our users and/or visitors are always aware of the type of
+              information we collect, how it will be used, and under what
+              circumstances if any, we may disclose such Information. If at any
+              point in time Digitide Education Private Limited decides to make
+              use of any personally identifiable information on file, in a
+              manner vastly different from that which was stated when this
+              information was initially collected, the user or users shall be
+              promptly notified by email. Users at that time shall have the
+              option as to whether or not to permit the use of their information
+              in this separate manner.
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">Acceptance of Terms</div>
+              Through the use of this website, you are hereby accepting the
+              terms and conditions stipulated within the aforementioned Privacy
+              Policy Agreement. If you are not in agreement with our terms and
+              conditions, then you should refrain from further use of our sites.
+              In addition, your continued use of our website following the
+              posting of any updates or changes to our terms and conditions
+              shall mean that you are in agreement and acceptance of such
+              changes.
+            </div>
+            <div className="text">
+              <div className="privacy-text-heading">How to Contact Us</div>
+              If you have any questions or concerns regarding the Privacy Policy
+              Agreement related to our website, please feel free to contact us
+              at the following email address:{" "}
+              <Link href="https://mail.google.com" className="privacy-anchor">
+                support@elalearn.com
+              </Link>{" "}
+              or{" "}
+              <Link href="https://mail.google.com" className="privacy-anchor">
+                admin@digitide.in
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Footer/> */}
+    </div>
+  );
+};
+
+export default Privacy;
---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Privacy from "../components/PrivacyPolicy/Privacy";
+
+const PrivacyPolicy = () => {
+  return <Privacy />;
+};
+
+export default PrivacyPolicy;
