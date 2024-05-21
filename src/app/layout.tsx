3be9a141f9999b9d84d25d4b4e5a23cf085c560c
janmenjaya <janmenjayabiswal@gmail.com>
2024-05-21 12:22:41 +0530

    added changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Quicksand } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 import Footers from "./footer/Footers";
 import Navbar from "./components/Navbar/Navbar";
-import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 const Quicksands = Quicksand({ subsets: ["latin"], weight: ["300", "600"] });
 export const metadata: Metadata = {
